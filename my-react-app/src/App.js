22bfc1ab10e1beaa1c876c049def94caca035d7b
szopian <szopiangreg@gmail.com>
2021-09-02 23:07:00 +0100

    add card img and email

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 import { CardList } from "./components/card-list/card-list";
 
 class App extends Component {
--- my-react-app/src/components/card/card.jsx
+++ my-react-app/src/components/card/card.jsx
@@ -4,7 +4,12 @@ import "./card.css";
 export const Card = (props) => {
   return (
     <div className="card-container">
-      <h1>{props.worker.name}</h1>
+      <img
+        alt="worker"
+        src={`https://robohash.org/${props.worker.id}?set=set2&size160x160`}
+      />
+      <h3>{props.worker.name}</h3>
+      <p>{props.worker.email}</p>
     </div>
   );
 };
