c1b739c22cb3f77213bc2f6720c80a5d26a2fbbb
szopian <szopiangreg@gmail.com>
2021-09-02 20:36:44 +0100

    create component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,24 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hey app</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: "Hey you!",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.string}</h1>
+        <button onClick={() => this.setState({ string: "Hey me!" })}>
+          Change Text
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
