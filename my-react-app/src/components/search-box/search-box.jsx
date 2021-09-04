18b989c8c95e6922bd6a563426b224e9d88ca498
szopian <szopiangreg@gmail.com>
2021-09-04 11:09:13 +0100

    add serach box component

--- my-react-app/src/components/search-box/search-box.jsx
+++ my-react-app/src/components/search-box/search-box.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+import "./search-box.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => {
+  return (
+    <input
+      type="search"
+      placeholder={placeholder}
+      onChange={(e) => this.setState({ handleChange })}
+    />
+  );
+};
